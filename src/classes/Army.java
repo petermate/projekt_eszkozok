50f66cf3d0ecc0398d57a13c68b0e1c2897990bd
posz <poszfilm@gmail.com>
2018-04-29 03:54:00 +0200

    seregszintű logika

--- src/classes/Army.java
+++ src/classes/Army.java
@@ -1,39 +1,63 @@
 package classes;
 
+import java.util.ArrayList;
 
 public class Army {
 	
+	public Leader leader;
 	public int manpower;
 	public int combatWidth;
 	public int infantryToCavalryRatio; // százalékosan, 0 és 100 között
 	public int cavalryPositionValueInOneSide; // X position to each side for the units of cavalry
-	public int artilleryPositionValue;
-
-	public int numberOfInfantry;
-	public int numberOfCavalry;
-	public int numberOfArtillery;
-	public int numberOfSpareUnit;
-	
-	Unit[] firstRow;
-	Unit[] secondRow;
+	public boolean isWonBattle;
 	
 	
-	public Army( int combatWidth, int cavalryPositionValueInOneSide, int artilleryPositionValue) {
+	ArrayList<CombatUnit> units;
+	ArrayList<CombatUnit> unitsOfInfantry;
+	ArrayList<CombatUnit> unitsOfCavalry;
+	ArrayList<CombatUnit> unitsOfArtillery;
+	
+	CombatUnit[] firstRow;
+	CombatUnit[] secondRow;
+	
+	public int numberOfSpareUnit; //tartalékok száma
+	public int NumberOfDeadInfantryUnit;
+	public int NumberOfDeadCavalryUnit;
+	public int NumberOfDeadArtilleryUnit;
+	public int NumberOfRetreatingInfantryUnit;
+	public int NumberOfRetreatingCavalryUnit;
+	public int NumberOfRetreatingArtilleryUnit;
+	
+	
+	public Army( Leader leader, int combatWidth, int cavalryPositionValueInOneSide, ArrayList<CombatUnit> units) {
+		
+		this.leader = leader;
 		this.manpower = 1000;
 		this.combatWidth = combatWidth;
 		this.infantryToCavalryRatio = 50;
 		this.cavalryPositionValueInOneSide = cavalryPositionValueInOneSide;
-		this.artilleryPositionValue = artilleryPositionValue;
-				
-		firstRow = new Unit[combatWidth];
-		secondRow = new Unit[combatWidth];
 		
-		numberOfArtillery = artilleryPositionValue;		
-		numberOfInfantry = ( manpower - numberOfArtillery ) * ( infantryToCavalryRatio / 100 ) ;
-		numberOfCavalry = ( manpower - numberOfArtillery ) - numberOfInfantry  ;
 		this.numberOfSpareUnit = 0;
+		this.NumberOfDeadInfantryUnit = 0;
+		this.NumberOfDeadCavalryUnit = 0;
+		this.NumberOfDeadArtilleryUnit = 0;		
+		this.NumberOfRetreatingInfantryUnit = 0;
+		this.NumberOfRetreatingCavalryUnit = 0;
+		this.NumberOfRetreatingArtilleryUnit = 0;
 		
-		deployUnits();
+		this.isWonBattle = false;
+				
+		firstRow = new CombatUnit[combatWidth];
+		secondRow = new CombatUnit[combatWidth];
+		
+		
+		this.units = new ArrayList<CombatUnit>();
+		
+		this.units = units;
+		
+		checkMoral( this.units );
+		splitUnits( this.units );		
+		deployUnitsToStartingPosition();
 		
 	}
 	 
@@ -41,7 +65,59 @@ public class Army {
 		
 	}
 	
-	public void deployUnits() {
+	public void splitUnits( ArrayList<CombatUnit> units ) {
+		
+		for( CombatUnit unit : units) {
+			
+			if( unit.type.equals(UnitType.INFANTRY) ) {
+				
+				unitsOfInfantry.add(unit);
+			}
+			else if( unit.type.equals(UnitType.CAVALRY) ) {
+				
+				unitsOfCavalry.add(unit);
+			}
+			else if( unit.type.equals(UnitType.ARTILLERY) ) {
+				
+				unitsOfArtillery.add(unit);
+			}
+		}
+		
+	}
+	
+	public void checkMoral( ArrayList<CombatUnit> units )  {
+		
+		ArrayList<CombatUnit> unitsToRemove= new ArrayList<CombatUnit>();
+		
+		for( CombatUnit unit : units) {
+			
+			if( unit.getMorale() == 0) {
+				
+				unitsToRemove.add(unit);
+				
+				if( unit.type.equals(UnitType.INFANTRY) ) {
+					
+					NumberOfRetreatingInfantryUnit++;
+				}
+				else if( unit.type.equals(UnitType.CAVALRY) ) {
+					
+					NumberOfRetreatingCavalryUnit++;
+				}
+				else if( unit.type.equals(UnitType.ARTILLERY) ) {
+					
+					NumberOfRetreatingArtilleryUnit++;
+				}
+				
+			}
+		}
+		
+		units.removeAll( unitsToRemove );
+		
+	}	
+	
+	
+	
+	public void deployUnitsToStartingPosition() {
 		
 		// first row
 		
@@ -58,50 +134,47 @@ public class Army {
 			
 		}
 		
-		setNumberOfSpareUnit( ( numberOfCavalry + numberOfArtillery + numberOfInfantry) );
+		setNumberOfSpareUnit( ( unitsOfCavalry.size() + unitsOfArtillery.size() + unitsOfInfantry.size() ) );
 	}	
 	
-	public void setRow( Unit[] row, int index ) {
+	
+	public void setRow( CombatUnit[] row, int index ) {
 		
 				
-		int artilleryIndexBegin = ( combatWidth / 2 ) - ( numberOfArtillery / 2 ); 
+		int artilleryIndexBegin = ( combatWidth / 2 ) - ( unitsOfArtillery.size() / 2 ); 
 		
 	
 		if( ( index < cavalryPositionValueInOneSide ) || ( index > combatWidth - cavalryPositionValueInOneSide) ) {
 			
-			if( numberOfCavalry != 0 ) {
-				
-				row[index] = new Unit();
-				row[index].setType(UnitType.CAVALRY);
-				setNumberOfCavalry( ( numberOfCavalry-1) );
+			if( !unitsOfCavalry.isEmpty() ) {
+								
+				row[index] = unitsOfCavalry.get(0);
+				unitsOfCavalry.remove(0);
 			}
 			else {
 				
-				if( numberOfInfantry != 0 ) {
+				if( !unitsOfInfantry.isEmpty() ) {
 					
-					row[index] = new Unit();
-					row[index].setType(UnitType.INFANTRY);
-					setNumberOfInfantry( ( numberOfInfantry-1) );	
+					row[index] = unitsOfInfantry.get(0);
+					unitsOfInfantry.remove(0);
 				}
 								
 			}
 			
 		}			
-		else if( ( row.equals(secondRow) ) && ( ( index >  artilleryIndexBegin ) || ( index < ( artilleryIndexBegin + numberOfArtillery ) ) ) ) {
+		else if( ( row.equals(secondRow) ) && ( ( index >  artilleryIndexBegin ) || ( index < ( artilleryIndexBegin + unitsOfArtillery.size() ) ) ) ) {
 			
-			if( numberOfArtillery != 0 ) {
+			if( !unitsOfArtillery.isEmpty() ) {
 				
-				row[index] = new Unit();
-				row[index].setType(UnitType.ARTILLERY);
-				setNumberOfArtillery( ( numberOfArtillery-1) );
+				row[index] = unitsOfArtillery.get(0);
+				unitsOfArtillery.remove(0);
 			}
 			else {
 				
-				if( numberOfInfantry != 0 ) {
+				if( !unitsOfInfantry.isEmpty() ) {
 					
-					row[index] = new Unit();
-					row[index].setType(UnitType.INFANTRY);
-					setNumberOfInfantry( ( numberOfInfantry-1) );	
+					row[index] = unitsOfInfantry.get(0);
+					unitsOfInfantry.remove(0);
 				}
 								
 			}
@@ -109,11 +182,10 @@ public class Army {
 		}
 		else {
 			
-			if( numberOfInfantry != 0 ) {
+			if( !unitsOfInfantry.isEmpty() ) {
 				
-				row[index] = new Unit();
-				row[index].setType(UnitType.INFANTRY);
-				setNumberOfInfantry( ( numberOfInfantry-1) );	
+				row[index] = unitsOfInfantry.get(0);
+				unitsOfInfantry.remove(0);
 			}
 			
 		}
@@ -121,8 +193,103 @@ public class Army {
 		
 	}
 	
-	
-	
+		
+	public void getNewUnitForPosition( CombatUnit unit ) {
+		
+				
+		if( numberOfSpareUnit != 0 ) {
+			
+			if( unit.getType().equals(UnitType.CAVALRY) ) {
+				
+				NumberOfRetreatingCavalryUnit++;
+				
+				if( !unitsOfCavalry.isEmpty() ) {
+					
+					unit = unitsOfCavalry.get(0);
+					unitsOfCavalry.remove(0);
+				}
+				else if( !unitsOfInfantry.isEmpty() ) {
+					
+					unit = unitsOfInfantry.get(0);
+					unitsOfInfantry.remove(0);
+				}
+				else if( !unitsOfArtillery.isEmpty() ){
+					
+					unit = unitsOfArtillery.get(0);
+					unitsOfArtillery.remove(0);
+				}			
+			
+			}
+			else if( unit.getType().equals(UnitType.INFANTRY) ) {
+				
+				NumberOfRetreatingInfantryUnit++;
+				
+				if( !unitsOfInfantry.isEmpty() ) {
+					
+					unit = unitsOfInfantry.get(0);
+					unitsOfInfantry.remove(0);
+				}
+				else if( !unitsOfCavalry.isEmpty() ) {
+					
+					unit = unitsOfCavalry.get(0);
+					unitsOfCavalry.remove(0);
+				}
+				else if( !unitsOfArtillery.isEmpty() ){
+					
+					unit = unitsOfArtillery.get(0);
+					unitsOfArtillery.remove(0);
+				}
+			
+			
+			}
+			else if( unit.getType().equals(UnitType.ARTILLERY) ) {
+				
+				NumberOfRetreatingArtilleryUnit++;
+				
+				if( !unitsOfInfantry.isEmpty() ) {
+					
+					unit = unitsOfInfantry.get(0);
+					unitsOfInfantry.remove(0);
+				}
+				else if( !unitsOfCavalry.isEmpty() ) {
+					
+					unit = unitsOfCavalry.get(0);
+					unitsOfCavalry.remove(0);
+				}
+				else if( !unitsOfArtillery.isEmpty() ){
+					
+					unit = unitsOfArtillery.get(0);
+					unitsOfArtillery.remove(0);
+				}
+			
+			
+			}
+			
+		
+		}
+		
+		setNumberOfSpareUnit( ( unitsOfCavalry.size() + unitsOfArtillery.size() + unitsOfInfantry.size() ) );
+	}
+		
+	public void switchPositions() {
+		
+		for( int indexRow=0; indexRow < combatWidth; indexRow++) {
+			
+			if( firstRow[indexRow].getMorale() == 0 ) {
+				
+				getNewUnitForPosition( firstRow[indexRow] );
+					
+			}	
+			
+			if( secondRow[indexRow].getMorale() == 0) {
+				
+				getNewUnitForPosition( secondRow[indexRow] );
+			}
+				
+		}
+					
+		
+	}
 	
 	
 	
@@ -145,27 +312,9 @@ public class Army {
 		return cavalryPositionValueInOneSide;
 	}
 	
-	public int getArtilleryPositionValue() {
-		return artilleryPositionValue;
-	}
-	
-	public int getNumberOfInfantry() {
-		return numberOfInfantry;
-	}
-	
-	public int getNumberOfCavalry() {
-		return numberOfCavalry;
-	}
-	
-	public int getNumberOfArtillery() {
-		return numberOfArtillery;
-	}
-	
-	
 	public int getNumberOfSpareUnit() {
 		return numberOfSpareUnit;
 	}
-	
 	
 	public void setManpower( int manpower ) {
 		this.manpower = manpower;
@@ -183,24 +332,78 @@ public class Army {
 		this.cavalryPositionValueInOneSide = cavalryPositionValueInOneSide;
 	}
 	
-	public void setArtilleryPositionValue( int artilleryPositionValue ) {
-		this.artilleryPositionValue = artilleryPositionValue;
-	}
-	
-	public void setNumberOfInfantry( int numberOfInfantry ) {
-		this.numberOfInfantry = numberOfInfantry;
-	}
-	
-	public void setNumberOfCavalry( int numberOfCavalry ) {
-		this.numberOfCavalry = numberOfCavalry;
-	}
-	
-	public void setNumberOfArtillery( int numberOfArtillery ) {
-		this.numberOfArtillery = numberOfArtillery;
-	}
-	
 	public void setNumberOfSpareUnit( int numberOfSpareUnit ) {
 		this.numberOfSpareUnit = numberOfSpareUnit;
+	}
+	
+	public int getNumberOfDeadInfantryUnit() {
+		
+		return NumberOfDeadInfantryUnit;
+	}
+	
+	public int getNumberOfDeadCavalryUnit() {
+		
+		return NumberOfDeadCavalryUnit; 
+	}
+	
+	public int getNumberOfDeadArtilleryUnit() {
+		
+		return NumberOfDeadArtilleryUnit;
+	}
+	
+	public int getNumberOfRetreatingInfantryUnit() {
+		
+		return NumberOfRetreatingInfantryUnit;
+	}
+	
+	public int getNumberOfRetreatingCavalryUnit() {
+		
+		return NumberOfRetreatingCavalryUnit;
+	}
+	
+	public int getNumberOfRetreatingArtilleryUnit() {
+		
+		return NumberOfRetreatingArtilleryUnit;
+	}
+	
+	public void setNumberOfDeadInfantryUnit( int NumberOfDeadInfantryUnit ) {
+		
+		this.NumberOfDeadInfantryUnit = NumberOfDeadInfantryUnit;
+	}
+	
+	public void setNumberOfDeadCavalryUnit( int NumberOfDeadCavalryUnit ) {
+		
+		this.NumberOfDeadCavalryUnit = NumberOfDeadCavalryUnit; 
+	}
+	
+	public void setNumberOfDeadArtilleryUnit( int NumberOfDeadArtilleryUnit ) {
+		
+		this.NumberOfDeadArtilleryUnit = NumberOfDeadArtilleryUnit;
+	}
+	
+	public void setNumberOfRetreatingInfantryUnit( int NumberOfRetreatingInfantryUnit ) {
+		
+		this.NumberOfRetreatingInfantryUnit = NumberOfRetreatingInfantryUnit;
+	}
+	
+	public void setNumberOfRetreatingCavalryUnit( int NumberOfRetreatingCavalryUnit ) {
+		
+		this.NumberOfRetreatingCavalryUnit = NumberOfRetreatingCavalryUnit;
+	}
+	
+	public void setNumberOfRetreatingArtilleryUnit( int NumberOfRetreatingArtilleryUnit ) {
+		
+		this.NumberOfRetreatingArtilleryUnit = NumberOfRetreatingArtilleryUnit;
+	}
+	
+	public boolean getIsWonBattle() {
+		
+		return isWonBattle;
+	}
+	
+	public void setIsWonBattle( boolean isWonBattle ) {
+		
+		this.isWonBattle = isWonBattle;
 	}
 	
 }
