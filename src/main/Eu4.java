69381f1c94dc98d6bb18ae276e99ad7e3a6fcf33
bentakacs <bencetakacs@outlook.com>
2018-04-29 09:47:26 +0200

    input paremeters from user

--- src/main/Eu4.java
+++ src/main/Eu4.java
@@ -1,9 +1,9 @@
 package main;
 
 public class Eu4 {
-
+		
 	public static void main(String[] args) {
-		System.out.println("EU4 csataszimulátor teszt");
+		StartGame sg = new StartGame();
+		sg.start();
 	}
-
 }
